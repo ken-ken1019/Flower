8060458da5fdbc344c406aee4a79137799c6018c
Ken <nerosecre94@gmail.com>
2025-10-16 12:54:38 +0800

    update

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 function bloomRose(rose) {
   rose.classList.toggle('bloom');
 
-  const music = window.bgMusic;
-  if (music && music.paused) {
-    music.play().catch(err => console.log("Autoplay blocked:", err));
-  }
-
   setTimeout(() => {
     window.location.href = "nextpage.html";
   }, 1500);
